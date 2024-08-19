41354ed08605a713fa449b50acda4b8975987300
Jesse Wachtel <84250138+blockchainTrucker@users.noreply.github.com>
2024-08-19 17:46:40 -0400

    working on linking sdk as native module

---
+++ android/src/main/java/com/geniusSDK/GeniusSDKModule.java
@@ -0,0 +1,27 @@
+package com.yourpackage;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class GeniusSDKModule extends ReactContextBaseJavaModule {
+
+    static {
+        System.loadLibrary("GeniusSDK"); // Load the native library
+    }
+
+    public GeniusSDKModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "GeniusSDKModule"; // This name is used to access the module from JavaScript
+    }
+
+    @ReactMethod
+    public void GeniusSDKInit(Callback callback) {
+        callback.invoke("GeniusSDKModule Initialized"); // Return the initialization string to JavaScript
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { Platform } from "./utils/platform";
+import { NativeModules } from "react-native";
 
 export const initGnus = (): void => {
   console.log("Initializing SDK...");
 
   const platform = Platform.getPlatform();
   console.log(`Running on platform: ${platform}`);
+
+  if (platform === "Android") {
+    const { MyNativeModule } = NativeModules;
+    if (MyNativeModule) {
+      MyNativeModule.getNativeStringFromSo((result: string) => {
+        console.log(`Native string from .so file: ${result}`);
+      });
+    }
+  } else if (platform === "iOS") {
+    // iOS-specific initialization code goes here
+  }
 };
